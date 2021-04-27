f28126995e41269396c813267e63e52882336413
infobip <infobip@infobips-MacBook-Pro.local>
2021-04-27 15:16:29 -0500

    delete  unnecessary old stuff

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ git submodule init
 git submodule update
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo source ~/.my-profile>> ~/.zshrc
+echo source ~/.my-profile>> ~/.oh-my-zsh
